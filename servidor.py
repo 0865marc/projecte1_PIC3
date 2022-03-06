ba55cdb7544264f71eaf1c7521fd4c759a1b0572
marc teixidó <marc teixid>
2022-03-06 22:44:42 +0100

    Ended servidor.py, added books and mails to the repo

--- servidor.py
+++ servidor.py
@@ -1,19 +1,52 @@
+#%%
 import socket
+import os
+import random
 
+#%%
 port = input("introdueix el port")
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind(("127.0.0.1", port))
 
-data, adress = socket.recvfrom(4096)
-
-msg = data.decode("UTF-8")
 
 
-if msg[0] == "1":
-    pass
-elif msg[0] == "1":
-    pass
-elif msg[0] == "2":
-    pass
+while True:
+    data, adress = socket.recvfrom(4096)
+
+    msg = data.decode("UTF-8")
+
+    if msg[0] == "1":
+        # Count number of mails
+        n_mails = len(os.listdir("mails"))
+        sent = socket.sendto(n_mails, adress)      ##convertir n_mails a bytes
+
+    elif msg[0] == "2":
+        # Return random mail
+        mails = os.listdir("mails")
+        random_mail = mails[random.randint(0, len(mails)-1)]
+        with open("mails/" + random_mail) as f:
+            content = f.read()
+        sent = socket.sendto(content, adress)      ##convertir content a bytes
+
+    elif msg[0] == "3":
+        # Count number of books
+        n_books = len(os.listdir("books"))
+        sent = socket.sendto(n_books, adress)      ##convertir n_books a bytes
+
+    elif msg[0] == "4":
+        # Return random book
+        books = os.listdir("books")
+        random_book = books[random.randint(0, len(books)-1)]
+        with open("mails/" + random_book) as f:
+            content = f.read()
+        sent = socket.sendto(content, adress)       ##convertir content a bytes
+
+    elif msg[0] == "5":
+        # Exit execution
+        sent = socket.sendto("Exited", adress)      #convertir "Exited" a bytes
+        break
+
+    else:
+        print("Error, try again")
 
