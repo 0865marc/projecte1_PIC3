fbc64022d40e33c06dea80eac77d7bc26d2bf6bb
AnnaGuillamet <guillametanna@gmail.com>
2022-04-25 17:43:34 +0200

    Finalitzat

--- client.py
+++ client.py
@@ -1,37 +1,59 @@
 import socket
 
+def check_input(number):
+    if int(number) in list(range(1,6)):
+        print("La opció triada pel client és valida : ", number)
+        return True
+    else:
+        print("La opció triada pel clien NO és valida : ", number)
+        return False
+
 ##Socket creation
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 ##Send data
 sock.connect(("127.0.0.10",10000))
 
-while True:
-    print("What do you want to do?\n1-Number of Mails\n2-Get a Mail\n3-Number of books\n4-Get a book\n5-Exit the program\n")
-    number=input()
-    i=1
-    print(i)
-    #Information received from the server
-    while 1<=i and i<=3:
-        sock.send(number.encode()) 
-        print("enviat")
+shouldContinue = True
+while shouldContinue:
+    # Loop until a valid choice by the user is given
+    while True:
+        print("\n------------------------------------------------")
+        number=input("What do you want to do?\n1-Number of Mails\n2-Get a Mail\n3-Number of books\n4-Get a book\n5-Exit the program\n")
+        isCorrect = check_input(number)
+        if isCorrect:
+            break
+
+    tries = 1
+
+    while tries<=3:
+        print("Intent numero: ", tries)
+        sock.send(number.encode("utf_8"))
         a = sock.recv(10000)
-        print("rebut")
-        msg = a.decode("UTF-8") 
-        print(i)
-        if len(a) > 0:
-            if msg == "Exited":
-                print('See you soon')
-                sock.close()
-                break
-            elif msg == "Error Message: Address malformed":
-                print(msg)
-                i+=1
-                print(i)
-            else:
-                print(msg)
-                i=6
-                print(i)
+        msg= a.decode("UTF-8") 
+        tries += 1
+
+        if msg != "Error Message: Address malformed" and msg != "Exited":
+            # Si la resposa NO és un error, printeja la resposta i surt del bucle per tornar a demanar quina será la següent accio
+            print("Resposta rebuda del router: ", msg)
+            break
+        
+        elif msg == "Exited":
+            sock.close()
+            print('See you soon')
+            shouldContinue = False
+            break
+
+        else:
+            print("Adress malformed")
+    
+
+
+
+        
+        
+
+
 
                 
 
--- servidor2.py
+++ servidor2.py
@@ -9,46 +9,42 @@ import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("127.0.0.2", 10000))
-
-
+print("Server ip: 127.0.0.2")
 
 while True:
     data, adress = sock.recvfrom(4096)
 
-    msg = data.decode("UTF-8")
+    msg = data.decode("utf_8")
+    print("Petició rebuda: ", msg)
     
-    if msg[0] == "1":
+    if msg == "1":
         # Count number of mails
         n_mails = len(os.listdir("mails"))
-        sent = sock.sendto(str(n_mails).encode(), adress)      ##convertir n_mails a bytes
+        sent = sock.sendto(str(n_mails).encode("utf_8"), adress)      ##convertir n_mails a bytes
 
-    elif msg[0] == "2":
+    elif msg == "2":
         # Return random mail
         mails = os.listdir("mails")
         random_mail = mails[random.randint(0, len(mails)-1)]
         with open("mails/" + random_mail) as f:
             content = f.read()
-        sent = sock.sendto(content.encode(), adress)      ##convertir content a bytes
+        sent = sock.sendto(content.encode("utf_8"), adress)      ##convertir content a bytes
 
-    elif msg[0] == "3":
+    elif msg == "3":
         # Count number of books
         n_books = len(os.listdir("books"))
-        sent = sock.sendto(str(n_books).encode(), adress)      ##convertir n_books a bytes
+        sent = sock.sendto(str(n_books).encode("utf_8"), adress)      ##convertir n_books a bytes
 
-    elif msg[0] == "4":
+    elif msg == "4":
         # Return random book
         books = os.listdir("books")
         random_book = books[random.randint(0, len(books)-1)]
-        with open("mails/" + random_book) as f:
+        with open("books/" + random_book) as f:
             content = f.read()
-        sent = sock.sendto(content.encode(), adress)       ##convertir content a bytes
+        sent = sock.sendto(content.encode("utf_8"), adress)       ##convertir content a bytes
 
-    elif msg[0] == "5":
+    elif msg == "5":
         # Exit execution
-        sent = sock.sendto("Exited".encode(), adress)      #convertir "Exited" a bytes
-        time.sleep(2)
+        sent = sock.sendto("Exited".encode("utf_8"), adress)      #convertir "Exited" a bytes
         break
-
-    else:
-        print("Error, try again")
-
+    
